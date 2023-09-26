de8023507e7adfdfccb8b2d578eeb15b5b65daf6
GulshanSS <sahugulshan686@gmail.com>
2023-09-26 20:36:10 +0530

    Added routing to have unique document at start

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,23 @@
 import TextEditor from "./TextEditor/TextEditor";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { v4 as uuid } from "uuid";
 
 const App = () => {
   return (
-    <>
-      <TextEditor />
-    </>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={<Navigate to={`/documents/${uuid()}`} replace />}
+        />
+        <Route path="/documents/:id" element={<TextEditor />} />
+      </Routes>
+    </Router>
   );
 };
 
