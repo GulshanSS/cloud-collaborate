3193eda0c6633cab71a1c13bcd0f53e441152aa7
GulshanSS <sahugulshan686@gmail.com>
2023-09-26 08:31:21 +0530

    Client: Initial Commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold text-orange-400">
+          Cloud Collaborate
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
