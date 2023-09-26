234f247f61ee642084be9dc42edc5a6e74b2f99d
GulshanSS <sahugulshan686@gmail.com>
2023-09-26 21:49:38 +0530

    Calling queries under socket connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,10 @@ dotenv.config();
 import config from "./config";
 
 import io from "socket.io";
+import {
+  findByIdOrCreateDocument,
+  saveDocument,
+} from "./services/document.service";
 
 const socket = new io.Server(config.PORT, {
   cors: {
@@ -12,12 +16,17 @@ const socket = new io.Server(config.PORT, {
 });
 
 socket.on("connection", (socket) => {
-  socket.on("get-document", (documentId: string) => {
-    const data = "";
+  socket.on("get-document", async (documentId: string) => {
+    const document = await findByIdOrCreateDocument(documentId);
     socket.join(documentId);
-    socket.emit("load-document", data);
+    socket.emit("load-document", document.content);
+
     socket.on("send-changes", (delta) => {
       socket.broadcast.to(documentId).emit("receive-changes", delta);
+    });
+
+    socket.on("save-changes", async (data: string) => {
+      await saveDocument(documentId, data);
     });
   });
 });
